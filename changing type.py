c068744fa2f633368604fcf7b3d7250a5d12c25f
Collins <collinsnjoku42@gmail.com>
2026-02-09 19:13:28 +0300

    Fuctions of strings,integers,float

--- changing type.py
+++ changing type.py
@@ -16,3 +16,60 @@ grade=50
 grade_float= float(grade)
 
 print(grade_float)
+print(type(grade_float))
+
+"""
+ STRING → For words
+ A string is used when you want to store text.
+
+ Examples of things that are strings:
+
+ Your name
+
+ A sentence
+
+ A phone number written as text
+
+ Anything inside quotes
+
+ Strings are for talking.
+
+"""
+
+"""
+INTEGER → For whole numbers
+An integer is used when you want to count things.
+
+Examples:
+
+Age
+
+Number of apples
+
+Score in a game
+
+Number of students
+
+Integers are for counting.
+
+"""
+
+"""
+FLOAT → For numbers with decimals
+A float is used when you need precision.
+
+Examples:
+
+Height
+
+Weight
+
+Money with cents
+
+Temperature
+
+Measurements
+
+Floats are for measuring.
+
+"""
