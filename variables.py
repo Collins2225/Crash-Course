d2d95dc54563a16ede494a10b391a2612613f947
Collins <collinsnjoku42@gmail.com>
2026-02-09 05:39:08 +0300

    variables

---
+++ variables.py
@@ -0,0 +1,17 @@
+"""
+A variable in python is a named storage location that holds a value.
+it allows you to store data and reuse it throughout your program.
+you assign a value to a variable using the = operator
+
+"""
+name= "Roboton"
+age = 20
+height = 1.9
+favourite_sport= "football"
+
+print(name)
+print(age)
+print(height)
+print(favourite_sport)
+
+
