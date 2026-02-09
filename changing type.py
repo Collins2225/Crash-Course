8db6df5e21f73bc770ef8e683b7d4ad620a40cdd
Collins <collinsnjoku42@gmail.com>
2026-02-09 18:55:41 +0300

    string ,integer and float

---
+++ changing type.py
@@ -0,0 +1,18 @@
+#Type conversion (also called type casting) is the process of changing a value from one data type
+#  to another using built‑in functions like str(), int(), and float().
+
+age = 20
+age_str = str(age)
+
+#print(type(age))
+
+
+num= 30
+num_int = int(num)
+
+#print(num)
+
+grade=50
+grade_float= float(grade)
+
+print(grade_float)
