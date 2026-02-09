4d1b079b6898a14b26eaacee1bdaf3ae7eb6ebf3
Collins <collinsnjoku42@gmail.com>
2026-02-09 05:19:28 +0300

    comments

---
+++ comments.py
@@ -0,0 +1,13 @@
+
+"""multiline comments
+comments are pieces of text in your code that Python completely ignores.
+they exist only for humans — to explain what your code is doing, why you wrote it that way, or to leave notes for your future self.
+"""
+
+# single line comments: this is my name
+name="Megatron"
+
+# single line comment:say powerup
+print("Powerup", name)
+
+
