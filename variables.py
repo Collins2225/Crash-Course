543526326a10db0a85838464bb7b99e723919d57
Collins <collinsnjoku42@gmail.com>
2026-02-09 06:23:06 +0300

    variables: string,integer,float

--- variables.py
+++ variables.py
@@ -9,9 +9,50 @@ age = 20
 height = 1.9
 favourite_sport= "football"
 
-print(name)
+"""
+A string is anything inside quotes.
+It’s like writing a word or sentence on paper.
+
+"Hello"
+"25"
+"Collins"
+
+"""
+name=str("collins")
+name= "collins"
+
+# INTEGER → Whole numbers
+# An integer is a number with no decimal point.
+
+# Like counting apples:
+1
+5
+100
+-3
+
+year=int(2026)
+year= 2026
+
+# FLOAT → Numbers with a decimal
+# A float is a number with a decimal point.
+
+# Like measuring water or height:
+
+1.5
+3.14
+0.0
+-2.7
+
+
+age = float(100)
+age = 10.5
+
+
+# print(name)
+# print(age)
+# print(height)
+# print(favourite_sport)
+
 print(age)
-print(height)
-print(favourite_sport)
 
 
