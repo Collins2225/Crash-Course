4a27e0daaaeb328a1927aece299ae4a7985b6c0a
Collins <collinsnjoku42@gmail.com>
2026-02-16 03:48:46 +0300

    length of a string

---
+++ Length of a string.py
@@ -0,0 +1,41 @@
+#Python uses a built‑in function called len() to count how many characters are in a string.
+
+len("Hello") #5
+
+len("Python")        # 6
+len("12345")         # 5
+len("Hi!")           # 3
+len("Hello world")   # 11  (space counts)
+len(" ")             # 1   (a single space)
+
+len("a b c")   # 5
+len("")   # 0
+
+len(123)      # error
+len(str(123)) # 3
+
+len("\n")   # 1
+
+len("\t")   # 1
+
+len("Python")
+
+len("Hello world")
+
+len("12345")
+
+len("Hi!")
+
+len("   ")
+
+len("")
+
+len("True")
+
+len("False ")
+
+len("A\nB")
+
+len(str(2026))
+
+print(len)
