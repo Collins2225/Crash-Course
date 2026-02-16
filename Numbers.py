4633900e0c6fd9b472249b9da58c915876844995
Collins <collinsnjoku42@gmail.com>
2026-02-16 03:15:54 +0300

    numbers

---
+++ Numbers.py
@@ -0,0 +1,46 @@
+print(4 + 3.0) # 7.0
+
+print(9 // 2) #4
+
+print(9 % 2 ) #1
+
+print(2 ** 5) #32
+
+print(int(8.99)) #8
+
+print(float("6") + 2)#8.0
+
+print(3 * True) # 4
+
+print(type(5/1))  # <class 'float'>
+
+print(type(5 // 1)) # <class 'int'>
+
+print(10/ 4 + 1) #3.5
+
+print(2 + 3 * 4)#14
+
+print((2 + 3) * 4)#20
+
+print(2 ** 3 ** 2) #512
+
+print(10 - 3 * 2 + 1) #5
+
+print(10 / 2 > 3 and 5 - 2 == 3)#True
+
+print(not 3 > 2)#False
+
+print(5 + 4 > 3 * 3)#False
+
+print(4 * 2 ** 2)#16
+
+print((4 * 2) ** 2)#64
+
+print(3 + 2 > 4 or 1)#True
+
+
+
+
+
+
+
